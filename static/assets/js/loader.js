a42697b17977e0d270b6a6819b4c08f9c47d1685
siddit <purusid@outlook.com>
2022-08-30 17:58:19 +0530

    update

--- static/assets/js/loader.js
+++ static/assets/js/loader.js
@@ -1,10 +1,10 @@
+function initvars() {
+    inituivars();
+}
+
 function init() {
     initvars();
     setTimeout(() => {
         document.getElementById('loader').style.display = 'none';
     }, 2000);
-}
-
-function initvars(){
-    inituivars();
 }
